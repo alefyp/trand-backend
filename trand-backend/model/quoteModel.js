cbbbde4515c4b01c63c842c3badbc59a8f75547c
Alefyp <alejndraprz@gmail.com>
2021-11-07 18:57:49 -0500

    Create Quote mongoose schema

---
+++ trand-backend/model/quoteModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const quoteSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  idType: {
+    type: String,
+    required: true,
+    enum: ['CC', 'CE', 'PASSPORT']
+  },
+  idNumber: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(v),
+      message: (email) => `${email.value} is not a a valid email`,
+    },
+  },
+  originCity: {
+    type: String,
+    required: true,
+  },
+  destinationCity: {
+    type: String,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    enum: ['Cash', 'Credit Card', 'Debit Card', 'Apple Pay', 'Other'],
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  declaredValue: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: false
+  },
+  tax: {
+    type: Number,
+    required: false
+  }
+})
+
+module.exports = mongoose.model('Quote', quoteSchema);
