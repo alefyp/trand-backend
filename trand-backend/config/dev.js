525e9b711e3e611d30a59f4117759d430be0fcac
Alefyp <alejndraprz@gmail.com>
2021-11-07 22:43:58 -0500

    Setup envs

---
+++ trand-backend/config/dev.js
@@ -0,0 +1,3 @@
+exports.port = process.env.PORT || process.env.PORT || 8080;
+exports.dbUrl = process.env.DB_URL || 'mongodb+srv://alefy:xyz2222@clusterburgerqueen.75nw1.mongodb.net/kargoru?retryWrites=true&w=majority';
+
